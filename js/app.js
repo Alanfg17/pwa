e3fdcabfad6c1f86cd7396f0e38144e57bdda633
Alanfg17 <alanferg123@gmail.com>
2021-09-20 17:46:17 -0500

    a

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
-
+ let url = window.location.href;
+ let swDirect = "/pwa/sw.js"
 
 if(navigator.serviceWorker){
     console.log('Service Worker Disponible')
+    if (url.includes('localhost')) {
+        swDirect='/sw.js'
+    }
+    
     navigator.serviceWorker.register('/sw.js')
 }else{
     console.log('Service Worker no disponible')
